17434af4c1642c483fe5ee3058a00077b33a0683
soham555-maker <sohammargaj55555@gmail.com>
2023-10-07 22:58:34 +0530

    video 8 completed (made more sense of the TextUtils website).

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import logo from '../../public/logo.png';
-import PropTypes from 'prop-types'
-
+import React from "react";
+import logo from "../../public/logo.png";
+import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
     <div>
-        {/* <div className="App-header">
+      {/* <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
@@ -14,8 +13,8 @@ export default function Navbar(props) {
           To get started, edit <code>src/App.js</code> and save to reload.<br></br>
           This is the edited <code>src/App.js</code>.
         </p> */}
-        
-        {/* <nav>
+
+      {/* <nav>
           <ul>
             <li><img src={logo} alt="" /></li>
             <li>Home</li>
@@ -32,39 +31,62 @@ export default function Navbar(props) {
           </p>
         </div>*/}
 
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/"><img src={logo} alt="" /> {props.title}</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="/">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Contacts</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark my-4">
+        <div className="container-fluid" >
+          <a className="navbar-brand" href="/">
+            <img src={logo} alt="" /> {props.title}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Contacts
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-primary" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
 Navbar.PropTypes = {
-    title: PropTypes.string.isRequired,
-  };
-  
+  title: PropTypes.string.isRequired,
+};
+
 Navbar.defaultProps = {
-    title: "give Title here",
-  };
+  title: "give Title here",
+};
--- src/Components/Textfield.js
+++ src/Components/Textfield.js
@@ -3,10 +3,29 @@ import PropTypes from "prop-types";
 
 export default function Textfield(prop) {
   const [text, setText] = useState("");
+  let wordsCnt = text.split(" ").length
+  let minuts = 0;
+  let seconds = 0;
+  text.split(" ").forEach(element => {
+    if (element === ""){
+        wordsCnt -=1;
+    };
+  });
+  minuts = Math.floor((0.85*wordsCnt)/60);
+  seconds = Math.floor((0.85*wordsCnt)%60);
+  let minCnt = 50*5;
+  if (minCnt>text.length){
+    minCnt=text.length;
+  }
   const onHandleUpperCase = () => {
     // console.log("onHandleUpperCase clicked");
     let newText = text.toUpperCase();
-    setText(newText)
+    setText(newText);
+  };
+  const onHandleLowerCase = () => {
+    // console.log("onHandleUpperCase clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
   };
   const onChangeTextArea = (event) => {
     setText(event.target.value);
@@ -14,25 +33,43 @@ export default function Textfield(prop) {
 
   return (
     <div>
-      <h1>{prop.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="8"
-          placeholder="enter your text here"
-          value={text}
-          onChange={onChangeTextArea}
-        ></textarea>
+      <div className="container">
+        <h1>{prop.heading}</h1>
+        <div className="mb-0">
+          <textarea
+            className="form-control my-4"
+            id="exampleFormControlTextarea1"
+            rows="8"
+            placeholder="enter your text here"
+            value={text}
+            onChange={onChangeTextArea}
+          ></textarea>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="btn btn-primary mx-1"
+            onClick={onHandleUpperCase}
+          >
+            Transform to UPERCASE.
+          </button>
+          <button
+            type="submit"
+            className="btn btn-primary mx-1"
+            onClick={onHandleLowerCase}
+          >
+            Transform to lowercase.
+          </button>
+        </div>
       </div>
-      <div>
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={onHandleUpperCase}
-        >
-          Transform to UPERCASE.
-        </button>
+      <div className="container my-4">
+        <h2>Your text summary</h2>
+        <p>
+            The above text has {wordsCnt} words and {text.length} characters.<br/>
+            Also, an slow reader requires {minuts} minutes and {seconds} seconds to read.
+        </p>
+        <h2>Preview</h2>
+        <p>{text.slice(0| {minCnt})}</p>
       </div>
     </div>
   );
